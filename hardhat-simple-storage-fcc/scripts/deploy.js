98da9bebc2f45dd1b6484c1b431ea1cf2529002c
Kohmei Kadoya <ajis.kkadoya@gmail.com>
2022-07-20 13:27:28 +0900

    Hardhat Part 1

---
+++ hardhat-simple-storage-fcc/scripts/deploy.js
@@ -0,0 +1,45 @@
+//imports -> main -> call main
+const { ethers, run, network } = require("hardhat");
+
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("Deploying Contract...");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.deployed();
+  console.log(`Deployed Contract to: ${simpleStorage.address}`);
+
+  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    //If on Rinkbey and key exists
+    console.log("Waiting for block confirmation...");
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+    console.log("Done");
+  }
+
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current Value: ${currentValue}`);
+
+  const res = await simpleStorage.store(707);
+  await res.wait(1);
+  const newValue = await simpleStorage.retrieve();
+  console.log(`New Value: ${newValue}`);
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
